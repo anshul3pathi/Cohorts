3ba61be0d1e93f35df8b5a33627b0b04c978bdab
anshul3pathi <anshul.3pathi@gmail.com>
2021-06-15 19:27:51 +0530

    done with the top level navigation of the app (step - 5)

--- app/src/main/java/com/example/cohorts/ui/MainActivity.kt
+++ app/src/main/java/com/example/cohorts/ui/MainActivity.kt
@@ -3,14 +3,38 @@ package com.example.cohorts.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.cohorts.R
 import com.example.cohorts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.cohortsFragment,
+            R.id.chatFragment,
+            R.id.filesFragment
+        ))
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        setBottomNavigationMenu(navController)
     }
+
+    private fun setBottomNavigationMenu(navController: NavController) {
+        val bottomNavigation = binding.bottomNavigation
+        bottomNavigation.setupWithNavController(navController)
+    }
+
 }
